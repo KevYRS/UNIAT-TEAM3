12a9d774426e57beed03dd75618b5f74813668b3
JimeTg98 <jimenatg98@gmail.com>
2024-11-13 13:03:08 -0600

    Parallax

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    [SerializeField] private float parallax_m;
+
+    private Transform cameraTransform;
+    private Vector3 previousCameraPosition;
+
+    private float spriteWidth, startPosition;
+
+
+    void Start()
+    {
+        cameraTransform = Camera.main.transform;
+        previousCameraPosition = cameraTransform.position;
+        spriteWidth = transform.position.x;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        float deltaX = (cameraTransform.position.x - previousCameraPosition.x) * parallax_m;
+        float moveAmount = cameraTransform.position.x * (1 - parallax_m);
+        transform.Translate(new Vector3(deltaX, 0, 0));
+        previousCameraPosition = cameraTransform.position;
+
+
+        if (moveAmount > startPosition + spriteWidth)
+        {
+            transform.Translate(new Vector3(spriteWidth, 0, 0));
+            startPosition += spriteWidth;
+
+        }
+
+        else if (moveAmount < startPosition - spriteWidth)
+        {
+
+            transform.Translate(new Vector3(-spriteWidth, 0, 0));
+            startPosition -= spriteWidth;
+
+        }
+
+    }
+}
